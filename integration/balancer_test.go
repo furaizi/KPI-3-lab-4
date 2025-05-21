6d92eaa9c8c4e84d38d9c95141514a5d034155a7
elaiviaien <skrypnyk.michael@gmail.com>
2025-05-21 11:08:21 +0300

    Add integration tests for lb

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,32 +1,64 @@
 package integration
 
 import (
-	"fmt"
-	"net/http"
-	"os"
-	"testing"
-	"time"
+  "fmt"
+  "net/http"
+  "testing"
+  "time"
+  "os"
 )
 
 const baseAddress = "http://balancer:8090"
 
 var client = http.Client{
-	Timeout: 3 * time.Second,
+  Timeout: 3 * time.Second,
 }
 
 func TestBalancer(t *testing.T) {
-	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+  if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
 		t.Skip("Integration test is not enabled")
-	}
+  }
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+  serverHits := make(map[string]int)
+  const requests = 20
+
+  // чекаємо, щоб сервери запустились
+  time.Sleep(6 * time.Second)
+
+  for i := 0; i < requests; i++ {
+    resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+    if err != nil {
+      t.Fatalf("Request failed: %v", err)
+    }
+    from := resp.Header.Get("lb-from")
+    resp.Body.Close() // закриваємо відповідь
+    if from == "" {
+      t.Errorf("Missing 'lb-from' header in response")
+      continue
+    }
+    t.Logf("Response from [%s]", from)
+    serverHits[from]++
+  }
+
+  t.Logf("Server hits distribution: %v", serverHits)
+
+  if len(serverHits) < 2 {
+    t.Errorf("Expected requests to hit at least 2 different servers, got %d", len(serverHits))
+  }
 }
 
+
+
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+  if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		b.Skip("Integration test is not enabled")
+  }
+
+  for i := 0; i < b.N; i++ {
+    resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+    if err != nil {
+      b.Fatalf("Request failed: %v", err)
+    }
+    resp.Body.Close()
+  }
 }
